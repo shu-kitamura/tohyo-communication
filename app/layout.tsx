4e7e09370aac383af3c57e24dbe7e07be579c9be
shu-kitamura <shusei3316@yahoo.co.jp>
2025-11-30 21:05:45 +0900

    set metadataBase

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"),
   title: "TOHYO通信 ~Vote Communication~",
   description: "プレゼンテーションやイベントで使用するリアルタイム投票Webアプリケーション",
 };
